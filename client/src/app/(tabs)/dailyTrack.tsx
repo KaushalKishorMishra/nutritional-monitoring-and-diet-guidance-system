d2ebd1b4567c33eeef71393fc02910e0d9c55753
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-09-30 07:45:03 +0545

    update: added simple pages and navigations to change teh tabs

---
+++ client/src/app/(tabs)/dailyTrack.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const TrackYourDiet = () => {
+  return (
+    <View>
+      <Text className="text-white">I am here</Text>
+    </View>
+  );
+};
+
+export default TrackYourDiet;
---
+++ client/src/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View className="px-5 ">
+      <Text className="text-white">Index Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
