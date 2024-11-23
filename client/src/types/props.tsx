5bde70fa217361b2365edfbcd3cedccb571768ae
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-11-23 08:00:08 +0545

    update: removed tailwind configuration from client

--- client/src/types/props.tsx
+++
@@ -1,6 +0,0 @@
-import type { PropsWithChildren, ReactElement } from "react";
-
-export type PParallaxScrollView = PropsWithChildren<{
-  headerImage: ReactElement;
-  headerBackgroundColor: { dark: string; light: string };
-}>;
