2f4b74101f499773d851f1d4db3ad4e59576ad9d
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2025-01-26 09:29:43 +0545

    history fetch

--- react-client/src/api/recommendation.api.ts
+++ react-client/src/api/recommendation.api.ts
@@ -16,3 +16,16 @@ export const getRecommendationByNutrition = async (date: Date) => {
     const responseData = response.data.payload.recommendation;
     return responseData;
 }
+
+export const getRecommendationByHistory = async () => {
+    const token = JSON.parse(localStorage.getItem("user-data")!).state.token;
+    const response = await axios.get(
+        `${BACKEND_API_URL}/user/recommend/history`, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }
+    );
+    const responseData = response.data.payload.recommendation;
+    return responseData;
+}
